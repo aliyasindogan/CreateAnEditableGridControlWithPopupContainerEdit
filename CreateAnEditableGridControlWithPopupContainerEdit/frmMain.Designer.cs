e2803d54ee8ac982ce0e63aaf1dca1eddd6bc3c5
Ali Yasin Doğan <a.yasindogan@gmail.com>
2020-04-25 02:29:17 +0300

    Add project files.

---
+++ CreateAnEditableGridControlWithPopupContainerEdit/frmMain.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace CreateAnEditableGridControlWithPopupContainerEdit
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.popupContainerEdit1 = new DevExpress.XtraEditors.PopupContainerEdit();
+            this.popupContainerControl1 = new DevExpress.XtraEditors.PopupContainerControl();
+            this.controlNavigator1 = new DevExpress.XtraEditors.ControlNavigator();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            ((System.ComponentModel.ISupportInitialize)(this.popupContainerEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.popupContainerControl1)).BeginInit();
+            this.popupContainerControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // popupContainerEdit1
+            // 
+            this.popupContainerEdit1.Location = new System.Drawing.Point(56, 38);
+            this.popupContainerEdit1.Name = "popupContainerEdit1";
+            this.popupContainerEdit1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.popupContainerEdit1.Size = new System.Drawing.Size(315, 20);
+            this.popupContainerEdit1.TabIndex = 1;
+            // 
+            // popupContainerControl1
+            // 
+            this.popupContainerControl1.Controls.Add(this.gridControl1);
+            this.popupContainerControl1.Controls.Add(this.controlNavigator1);
+            this.popupContainerControl1.Location = new System.Drawing.Point(56, 82);
+            this.popupContainerControl1.Name = "popupContainerControl1";
+            this.popupContainerControl1.Size = new System.Drawing.Size(315, 231);
+            this.popupContainerControl1.TabIndex = 2;
+            // 
+            // controlNavigator1
+            // 
+            this.controlNavigator1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.controlNavigator1.Location = new System.Drawing.Point(0, 207);
+            this.controlNavigator1.Name = "controlNavigator1";
+            this.controlNavigator1.Size = new System.Drawing.Size(315, 24);
+            this.controlNavigator1.TabIndex = 2;
+            this.controlNavigator1.Text = "controlNavigator1";
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl1.Location = new System.Drawing.Point(0, 0);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(315, 207);
+            this.gridControl1.TabIndex = 3;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(56, 13);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(63, 13);
+            this.labelControl1.TabIndex = 3;
+            this.labelControl1.Text = "Seçim Yapınız";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 356);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.popupContainerControl1);
+            this.Controls.Add(this.popupContainerEdit1);
+            this.Name = "frmMain";
+            this.Text = "PopupContainerEdit ile Düzenlenebilir Bir GridControl Oluşturun";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.popupContainerEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.popupContainerControl1)).EndInit();
+            this.popupContainerControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.PopupContainerEdit popupContainerEdit1;
+        private DevExpress.XtraEditors.PopupContainerControl popupContainerControl1;
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraEditors.ControlNavigator controlNavigator1;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+    }
+}
---
+++ CreateAnEditableGridControlWithPopupContainerEdit/frmMain.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace CreateAnEditableGridControlWithPopupContainerEdit
+{
+    public partial class frmMain : DevExpress.XtraEditors.XtraForm
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            gridControl1.DataSource = FillDataTable();
+            controlNavigator1.NavigatableControl = gridControl1;
+
+            popupContainerControl1.Controls.Add(gridControl1);
+            popupContainerControl1.Controls.Add(controlNavigator1);
+
+            popupContainerEdit1.Properties.PopupControl = popupContainerControl1;
+            popupContainerEdit1.Properties.ShowPopupCloseButton = false;
+            popupContainerEdit1.QueryDisplayText += new DevExpress.XtraEditors.Controls.QueryDisplayTextEventHandler(popupContainerEdit1_QueryDisplayText);
+            popupContainerEdit1.QueryResultValue += new DevExpress.XtraEditors.Controls.QueryResultValueEventHandler(popupContainerEdit1_QueryResultValue);
+
+            this.gridView1.Columns[0].MaxWidth = 30; //Id
+            this.gridView1.Columns[1].MaxWidth = 120; //AdSoyad
+            this.gridView1.Columns[2].MaxWidth = 30; //AdSoyad
+        }
+
+        private void popupContainerEdit1_QueryResultValue(object sender, DevExpress.XtraEditors.Controls.QueryResultValueEventArgs e)
+        {
+            e.Value = gridView1.GetFocusedRowCellValue("AdSoyad");
+        }
+
+        private void popupContainerEdit1_QueryDisplayText(object sender, DevExpress.XtraEditors.Controls.QueryDisplayTextEventArgs e)
+        {
+            e.DisplayText = e.EditValue != null ? e.EditValue.ToString() : "";
+        }
+
+        private DataTable FillDataTable()
+        {
+            DataTable _dataTable = new DataTable();
+            DataColumn col;
+            DataRow row;
+
+            col = new DataColumn();
+            col.ColumnName = "Id";
+            col.DataType = System.Type.GetType("System.Int32");
+            _dataTable.Columns.Add(col);
+
+            col = new DataColumn();
+            col.ColumnName = "AdSoyad";
+            col.DataType = System.Type.GetType("System.String");
+            _dataTable.Columns.Add(col);
+
+            col = new DataColumn();
+            col.ColumnName = "Aktif";
+            col.DataType = System.Type.GetType("System.Boolean");
+            _dataTable.Columns.Add(col);
+
+            row = _dataTable.NewRow();
+            row["Id"] = "1"; row["AdSoyad"] = "Ali Doğan"; row["Aktif"] = true;
+            _dataTable.Rows.Add(row);
+
+            row = _dataTable.NewRow();
+            row["Id"] = "2"; row["AdSoyad"] = "Mehmet Yılmaz"; row["Aktif"] = false;
+            _dataTable.Rows.Add(row);
+
+            row = _dataTable.NewRow();
+            row["Id"] = "3"; row["AdSoyad"] = "Recep Mert"; row["Aktif"] = true;
+            _dataTable.Rows.Add(row);
+
+            return _dataTable;
+        }
+    }
+}
